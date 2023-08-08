ad2a88054e496d1ac37b1b5e52c4a114759abe8f
Joles-Doe <105786625+Joles-Doe@users.noreply.github.com>
2023-08-08 13:44:36 +0100

    added replay, score

--- Assets/Game manager/GAME_MANAGER.cs
+++ Assets/Game manager/GAME_MANAGER.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GAME_MANAGER : MonoBehaviour
 {
-    public GameObject Endscreen;
+    public GameObject Menuscreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        Endscreen.SetActive(false);
+        Menuscreen.SetActive(false);
         Time.timeScale = 1;
     }
 
     public void GameOver()
     {
-        Endscreen.SetActive(true);
+        Menuscreen.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(0);
     }
 }
