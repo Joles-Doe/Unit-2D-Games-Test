5873c2aba785c81b9d4cafed1518c0cf122967b9
Joles-Doe <105786625+Joles-Doe@users.noreply.github.com>
2023-08-09 13:21:16 +0100

    reworked menu

--- Assets/Game manager/GAME_MANAGER.cs
+++ Assets/Game manager/GAME_MANAGER.cs
@@ -2,20 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public static class PlayerStats
+{
+    public static bool start = false;
+}
 
 public class GAME_MANAGER : MonoBehaviour
 {
     public GameObject Menuscreen;
+    public GameObject Gameoverscreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        Menuscreen.SetActive(false);
-        Time.timeScale = 1;
+        if (PlayerStats.start == false)
+        {
+            Debug.Log("start");
+            PlayerStats.start = true;
+            Menuscreen.SetActive(true);
+            Gameoverscreen.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Debug.Log("Start true");
+            Menuscreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 
     public void GameOver()
     {
+        Gameoverscreen.SetActive(true);
         Menuscreen.SetActive(true);
         Time.timeScale = 0;
     }
